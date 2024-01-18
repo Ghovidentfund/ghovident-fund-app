2fd1549357bd0393b61b3545a96269ba5ff23dcc
Teeraphat Sittinantakul <dev.teeraphat@gmail.com>
2024-01-18 12:47:40 +0700

    ✨ feat | Ghovident fund

--- src/providers/WrapConnectKitProvider.tsx
+++ src/providers/WrapConnectKitProvider.tsx
@@ -11,16 +11,17 @@ const config = createConfig(
   getDefaultConfig({
     // Required API Keys
     alchemyId: process.env.ALCHEMY_ID, // or infuraId
-    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
+    walletConnectProjectId:
+      process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
 
     // Chains
     chains,
 
     // Required
-    appName: "Your App Name",
+    appName: "Ghovident fund",
 
     // Optional
-    appDescription: "Your App Description",
+    appDescription: "Provident fund",
     appUrl: "https://family.co", // your app's url
     appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
   })

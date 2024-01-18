bbc5e1b4cbf0ae67d4c6de9afd9159c148759d1f
Teeraphat Sittinantakul <dev.teeraphat@gmail.com>
2024-01-18 11:37:25 +0700

    🐳 chore | add connectKit's family

---
+++ src/views/Connect/index.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ConnectKitButton } from "connectkit";
+
+const ConnectView = () => {
+  return (
+    <div>
+      <ConnectKitButton showBalance />
+    </div>
+  );
+};
+
+export default ConnectView;

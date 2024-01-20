6ff1b18caaa064698e7bff66006b12910a1f9c76
Teeraphat Sittinantakul <dev.teeraphat@gmail.com>
2024-01-20 23:05:10 +0700

    ✨ feat | footer

--- src/views/Connect/index.tsx
+++ src/views/Connect/index.tsx
@@ -46,7 +46,7 @@ const ConnectView = () => {
         </section>
       </div>
       <div className="fixed bottom-0 flex justify-center w-full bg-primary">
-        <div className="container mx-auto flex justify-between items-center py-3 min-h-16 text-white text-base">
+        <div className="container mx-auto flex justify-between items-center py-3 min-h-12 text-white text-xs">
           Copyright &#169; Ghovident 2024. All Rights Reserved.
         </div>
       </div>

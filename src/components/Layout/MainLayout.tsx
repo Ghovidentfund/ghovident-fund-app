befedb5b3c558546f95d08f850dc6895791850b1
Noppawan Saeong <noppawansa29@gmail.com>
2024-01-20 16:55:45 +0700

    chore | create footer

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Common/Navbar/Navbar";
+import Footer from "@/components/Common/Footer/Footer";
 
 interface MainLayoutProps {
   children: React.ReactNode;
@@ -9,6 +10,7 @@ const MainLayout = ({ children }: MainLayoutProps) => {
     <div className="min-h-screen relative">
       <Navbar />
       <main>{children}</main>
+      <Footer />
     </div>
   );
 };

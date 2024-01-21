1a7be665a2be92b3f34886728c80eaba3340a888
Jetnipat-c <alonecheer@hotmail.com>
2024-01-21 15:18:15 +0700

    ✨ feat: ABI

---
+++ src/constants/fundAbi.ts
@@ -0,0 +1,48 @@
+export const fundAbi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "assets",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "supply",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "assets",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "receiver",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "withdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
---
+++ src/constants/ghovidentPoolAbi.ts
@@ -0,0 +1,270 @@
+export const ghovidentPoolAbi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_factory",
+        type: "address",
+      },
+      {
+        internalType: "string",
+        name: "_name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_logoUri",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_factSheetUri",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "_risk",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_period",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "_assets",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "_fund",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "assets",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "factSheetUri",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "factory",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "fund",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_company",
+        type: "address",
+      },
+    ],
+    name: "getPoolInfo",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "string",
+            name: "name",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "logoUri",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "factSheetUri",
+            type: "string",
+          },
+          {
+            internalType: "uint256",
+            name: "risk",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "period",
+            type: "uint256",
+          },
+          {
+            internalType: "address",
+            name: "assets",
+            type: "address",
+          },
+          {
+            internalType: "address",
+            name: "fund",
+            type: "address",
+          },
+          {
+            internalType: "uint256",
+            name: "totalVolume",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "volume",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct IGhovidentPool.PoolInfo",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "logoUri",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "period",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "risk",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "supply",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalVolume",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "volumeOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
